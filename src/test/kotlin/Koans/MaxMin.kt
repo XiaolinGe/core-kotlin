8056dc4b78349d289c7d8d2e763874b6fe833fdb
Xiaolin Ge <xiaolinge@Xiaolins-MacBook-Pro.local>
2018-02-20 18:06:10 +1300

    create MaxMin

---
+++ src/test/kotlin/Koans/MaxMin.kt
@@ -0,0 +1,14 @@
+package Koans
+
+import org.junit.Assert
+import org.junit.Test
+
+class TestMaxMin {
+    @Test fun testCustomerWithMaximumNumberOfOrders() {
+        Assert.assertEquals(customers[reka], shop.getCustomerWithMaximumNumberOfOrders())
+    }
+
+    @Test fun testTheMostExpensiveOrderedProduct() {
+        Assert.assertEquals(rubyMine, customers[nathan]!!.getMostExpensiveOrderedProduct())
+    }
+}
